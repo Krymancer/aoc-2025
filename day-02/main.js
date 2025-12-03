8c009faa30c6c66838529d6b63ff27644550b948
Junior Nascimento <krymancer@protonmail.com>
2025-12-03 02:36:28 -0300

    Refactor variable declarations to use 'let' for ranges, seqLens, invIDs, and sum; enhance logic for generating unique IDs and summing values within specified ranges.

--- day-02/main.js
+++ day-02/main.js
@@ -2,7 +2,7 @@ import fs from "fs";
 
 let max = 0n;
 
-const ranges = fs.
+let ranges = fs.
 readFileSync("input.txt", "utf-8")
 .trim()
 .split(",")
@@ -15,12 +15,12 @@ readFileSync("input.txt", "utf-8")
 
 const L = Math.ceil(max.toString().length/2) * 2;
 
-const seqLens = Array.from(
+let seqLens = Array.from(
   {length: L/2},
   (_, i) => i + 1
 );
 
-const invIDs = seqLens.flatMap(S => {
+let invIDs = seqLens.flatMap(S => {
   const minS = BigInt(10) ** BigInt(S - 1);
   const maxS = (BigInt(10) ** BigInt(S)) - 1n;
 
@@ -31,7 +31,68 @@ const invIDs = seqLens.flatMap(S => {
   return seqs.map(s => BigInt(`${s}${s}`));
 });
 
-const sum = invIDs.reduce((acc, val) => {
+let sum = invIDs.reduce((acc, val) => {
+  const inRange = ranges.some(([start,end]) => {
+    return val >= start && val <= end;
+  });
+
+  return inRange ? acc + val : acc;
+}, 0n);
+
+console.log(`${sum}`);
+
+max = 0n;
+
+ranges = fs.
+readFileSync("input.txt", "utf-8")
+.trim()
+.split(",")
+.map(range => {
+  const [start, end] = range.split("-");
+  const maxCandidate = BigInt(end);
+  if (maxCandidate > max) max = maxCandidate;
+  return [BigInt(start), maxCandidate];
+});
+
+const maxIdLength = max.toString().length;
+const maxSeqLen = Math.floor(maxIdLength / 2);
+
+seqLens = Array.from(
+  {length: maxSeqLen},
+  (_, i) => i + 1
+);
+
+invIDs = seqLens.flatMap(S_len => {
+  const minS = BigInt(10) ** BigInt(S_len - 1);
+  const maxS = (BigInt(10) ** BigInt(S_len)) - 1n;
+
+
+  const seqs = [];
+  for(let s = minS; s <= maxS; s++) seqs.push(s);
+
+  return seqs.flatMap(s => {
+      const str = s.toString();
+      const generatedIDs = [];
+      let n = str; 
+
+      for (let R = 2; ; R++) {
+          n += str;
+
+          if (n.length > maxIdLength) break;
+
+          const val = BigInt(n);
+          
+          if (val > max) break;
+          
+          generatedIDs.push(val);
+      }
+      return generatedIDs;
+  });
+});
+
+const uniqueInvIDs = Array.from(new Set(invIDs));
+
+sum = uniqueInvIDs.reduce((acc, val) => {
   const inRange = ranges.some(([start,end]) => {
     return val >= start && val <= end;
   });
